710a3ccf6393f348bd20d3dd8a4a458081166122
Fona <51896217+Erhabor-Fona@users.noreply.github.com>
2022-09-26 15:14:09 +0100

    Create emem.h

---
+++ 0x07-pointers_arrays_strings/emem.h
@@ -0,0 +1,13 @@
+#ifndef EMEM_H
+#define EMEM_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+void set_string(char **s, char *to);
+
+#endif
